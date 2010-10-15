50e68abf3552113186b41fafb5ce7b35a2cd1cd7
Nerian <siotopo@gmail.com>
2010-10-16 01:10:06 +0200

    Refactoring of worker_spec.rb

--- worker/spec/worker/worker_spec.rb
+++ worker/spec/worker/worker_spec.rb
@@ -3,19 +3,11 @@ require 'spec_helper'
 module Worker
   describe Worker do
     describe "#add_job" do   
-
-      it "sends a info message" do
-
-        output = double('output')   
-        
-        output.should_receive(:puts).with('Received a new Job')
-        #Create a worker
-        worker = Worker.new(output)                            
-
-        # We set an expectation, something that should 
-        # happen before the end of this example
-                                   
-        worker.add_job("-w320 -h120", "povray.pov")
+      let(:output) {double('output')}  
+      let(:worker) {Worker.new(output)}
+      it "sends a info message" do                
+        output.should_receive(:puts).with('Received a new Job')                     
+        worker.add_job("-w320 -h120", "povray.pov")        
       end            
     end    
   end
