7db5a36ba232a5d489818180f57637d1fc51b937
Nerian <siotopo@gmail.com>
2010-10-16 21:12:02 +0200

    I changed the 'it' text of rspec test worker_spec.rb to make it more readable

--- worker/spec/worker/worker_spec.rb
+++ worker/spec/worker/worker_spec.rb
@@ -6,44 +6,44 @@ module Worker
     let(:job) {Job::Job.new('project_name', '-w320 -h120', 'povray.pov', 'partial_image_file_name')}  
     let(:worker) {Worker.new(output)}
         
-    describe "#add_job" do          
-      it "sends a received message" do              
+    describe "#add_job(job)" do          
+      it "Worker received a new Job" do              
         output.should_receive(:puts).with('Received a new Job')
         worker.add_job(job.serialize())        
       end                                                      
       
-      it "sends a unmarshaling the Job message" do 
+      it "Worker try to unmarshal the received Job" do 
         output.should_receive(:puts).with('Unmarshaling the Object...')
         worker.add_job(job.serialize())
       end 
       
-      it "sends a unmarshaling completion message" do
+      it "Worker succefully unmarshaled the job" do
         output.should_receive(:puts).with('Unmarshaling completed')
         worker.add_job(job.serialize())
       end
     end  
     
-    describe "#retrieve_pov_file_from_server" do
-      it "Ask for the .pov file" do
+    describe "#retrieve_pov_file_from_server()" do
+      it "Worker ask for the .pov file" do
         output.should_receive(:puts).with('Asking for .pov file') 
         worker.add_job(job.serialize())
         worker.retrieve_pov_file_from_server()       
       end
       
-      it "receive marshaled scene file" do
+      it "Worker received marshaled scene file" do
         output.should_receive(:puts).with("Received marshaled .Pov file")
         worker.add_job(job.serialize())
         worker.retrieve_pov_file_from_server()
         
       end
       
-      it "send message 'Unmarshaling .pov file'" do 
+      it "Worker is triying to unmarshal pov file" do 
         output.should_receive(:puts).with("Unmarshaling .pov file")
         worker.add_job(job.serialize())
         worker.retrieve_pov_file_from_server()
       end      
       
-      it "send message that the file is ready" do
+      it "Worker succefully unmarshaled the pov file" do
         output.should_receive(:puts).with("Pov file ready")
         worker.add_job(job.serialize())
         worker.retrieve_pov_file_from_server()        
