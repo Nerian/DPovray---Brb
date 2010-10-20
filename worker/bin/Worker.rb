c8b58ed1a08140773c05ef0ac499dbc5573e10c1
Nerian <siotopo@gmail.com>
2010-10-20 17:09:23 +0200

    Implement Worker bin example. This doesnt use brb connection yet, but the worker works perfectly. Brb functionality could be added easily. The Project_server object could be retrieved using BrB and passed to the Worker. We can fork the process and have many workers.

--- worker/bin/Worker.rb
+++ worker/bin/Worker.rb
@@ -1,11 +1,15 @@
+#!/usr/bin/env ruby
+
+
 # Author:: Gonzalo Rodríguez-Baltanás Díaz  
 # Licence:: See Licence.rdoc
-
-#!/usr/bin/env ruby
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__) 
 require 'worker_node'                        
 
 worker = Worker::Worker.new() 
-job = Job::Job.new('project_name', '-w320 -h120', 'povray.pov', 'partial_image_file_name')       
-
-worker.add_job(job.serialize())
+job = Job::Job.new('project_name', '-w320 -h120', 'povray.pov', 'partial_image_file_name')
+project_server = ProjectServer::ProjectServer.new       
+worker.add_job(job.serialize(), project_server)                                                                  
+worker.retrieve_pov_file_from_server()
+worker.povray_start_render()
+worker.send_rendered_image_to_job_requester()
