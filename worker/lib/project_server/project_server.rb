2b67b34bf3a0e7e336773189ded463df09d6efd0
Nerian <siotopo@gmail.com>
2010-10-20 17:06:36 +0200

    Add error messages to Project class in method put. This way the user can know if there was an error

--- worker/lib/project_server/project_server.rb
+++ worker/lib/project_server/project_server.rb
@@ -14,14 +14,18 @@ module ProjectServer
       marshaled_povray_scene_file = Marshal.dump(povray_scene_string)
     end
 
-    def put(marshaled_job, marshaled_image_file)
+    def put(marshaled_job, marshaled_image_file)      
       image_file_string = Marshal.load(marshaled_image_file)
       job = Marshal.load(marshaled_job)
+      return_value = nil
       if image_file_string.size == 0 or job.nil?        
-        false
+        return_value = false
+        puts "Error: Received 'put' order to register a completed job, but either Image or Job was corrupted"
       else          
-        true
+        return_value = true
+        puts "Image received"        
       end
+      return return_value
     end
   end
   
