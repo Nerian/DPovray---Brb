eb9945529699eaaf7a867afd4c4265a0b5c829b5
Nerian <siotopo@gmail.com>
2010-10-29 16:10:40 +0200

    Write Cucumber feature about the worker manager

---
+++ features/WorkerManager.feature
@@ -0,0 +1,29 @@
+Feature: WorkerManager use a worker to render a scene
+	In order to be able to render a scene
+	As a Worker Manager
+	I want to be able to create many workers and assign them jobs.
+
+
+Scenario Outline: Worker Manager split the job in many pieces among workers. 
+	Given I can use <cores> number of cores
+	And I have received job <job>   
+	When I start working on those jobs
+	Then I should split the job <number_of_jobs> times       
+	And I should see <job_division_result>
+	And I should create <number_workers> workers
+	And I should receive <number_workers> Partial Images
+
+	Examples:
+		| cores | job | number_of_jobs | job_division_result | number_workers |
+		| 1 | {'id' => 'projectName.part1', 'sc' => '0%','ec'=>'100%' } | 2 | {'1'=>{'sc'=>0% , 'ec'=>50%}, '2'=>{'sc'=>'50%', 'ec' => '100%'}} | 1 |
+		| 2 | {'id' => 'projectName.part1', 'sc' => '0%','ec'=>'100%' } | 4 | {'1'=>{'sc'=>0% , 'ec'=>25%}, '2'=>{'sc'=>'25%', 'ec' => '50%'}, '3'=>{'sc'=>'50%' , 'ec'=>'75%'}, '4'=>{'sc'=>'75%' , 'ec'=>'100%'}} | 1 |
+
+
+
+
+
+
+
+
+
+
---
+++ features/step_definitions/worker_manager.rb
@@ -0,0 +1,33 @@
+Given /^I can use (\d+) number of cores$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I have received job \{'id' => 'projectName\.part(\d+)', 'sc' => '(\d+)%','ec'=>'(\d+)%' \}$/ do |arg1, arg2, arg3|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I start working on those jobs$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should split the job (\d+) times$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+
+
+Then /^I should create (\d+) workers$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should receive (\d+) Partial Images$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see \{'(\d+)'=>\{'sc'=>(\d+)% , 'ec'=>(\d+)%\}, '(\d+)'=>\{'sc'=>'(\d+)%', 'ec' => '(\d+)%'\}, '(\d+)'=>\{'sc'=>'(\d+)%' , 'ec'=>'(\d+)%'\}, '(\d+)'=>\{'sc'=>'(\d+)%' , 'ec'=>'(\d+)%'\}\}$/ do |arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see \{'(\d+)'=>\{'sc'=>(\d+)% , 'ec'=>(\d+)%\}, '(\d+)'=>\{'sc'=>'(\d+)%', 'ec' => '(\d+)%'\}\}$/ do |arg1, arg2, arg3, arg4, arg5, arg6|
+  pending # express the regexp above with the code you wish you had
+end
