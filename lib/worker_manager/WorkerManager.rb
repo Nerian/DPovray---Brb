e40a5f6791aeb31d77b52081f0975ea0edebbfde
Nerian <siotopo@gmail.com>
2010-10-30 19:07:46 +0200

    Worker_manager receive jobs. Rspec test done

--- lib/worker_manager/WorkerManager.rb
+++ lib/worker_manager/WorkerManager.rb
@@ -1,17 +1,22 @@
 module WorkerManager
   class WorkerManager
     
-    attr_accessor :cores
+    attr_accessor :cores, :job
     
     def initialize(number_of_cores)
       unless number_of_cores.to_i > 0  
         raise ArgumentError, 'Number of cores must be a positive number'
       end               
-      @cores = number_of_cores     
+      @cores = number_of_cores
+      @job = nil     
     end                       
     
-    
-        
+    def addJob(job)           
+      if @job.nil?
+        @job = job      
+      else
+        raise ArgumentError, "Error: WorkerManager already got a Job assigned. It can only manage one at a time" 
+      end
+    end                
   end
-
 end
--- spec/worker_manager/WorkerManager_spec.rb
+++ spec/worker_manager/WorkerManager_spec.rb
@@ -20,15 +20,29 @@ module WorkerManager
       end                  
     end
     
-    describe "#Receive a Job" do
-      
-      it "Receive a job" do                               
-        number_of_cores = 1
+    describe "#Receive a Job" do      
+      it "Receive a job and @job was nil" do                               
+        number_of_cores = 1              
+        job = Job::Job.new("project:4", 0, 100)
+        
         worker_manager = WorkerManager.new(number_of_cores)        
-        worker_manager.addJob(Job::Job.new("project:4", 0, 100))
+        worker_manager.addJob(job)
+        worker_manager.job.should be(job)
       end
-      
-    end
-                             
+      it "Receive a job when already had one" do
+        number_of_cores = 1
+        job1 = Job::Job.new("project:4", 0, 100)
+        job2 = Job::Job.new("project:5", 0, 100) 
+        
+        worker_manager = WorkerManager.new(number_of_cores)        
+        worker_manager.addJob(job1)
+        worker_manager.job.should be(job1)
+        
+        lambda {worker_manager.addJob(job2)}.should raise_error("Error: WorkerManager already got a Job assigned. It can only manage one at a time")          
+        
+        worker_manager.job.should be(job1)    
+        
+      end      
+    end                             
   end
 end
